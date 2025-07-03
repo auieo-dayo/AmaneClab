5bdf2bc5ab139cb2f747c3b20940a0e548d4b675
auieo-dayo <yud.nitayama@gmail.com>
2025-07-03 23:16:46 +0900

    隠し機能の修正

--- js/global.js
+++ js/global.js
@@ -6,7 +6,6 @@ document.getElementById("title").style.color = color[0][color[1]]
 color[1]++
 },350)
 setTimeout(()=>{
-    document.title.textContent = document.title.textContent.replace("あまね部","あまネギ")
     document.getElementById("title").textContent = "あまネギ"
     alert("おや？あまね部の様子が....")
 },1000*60*1)
